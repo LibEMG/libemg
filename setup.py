2d81e9fdd31f9e4604f621124614bae2a139f9c6
ECEEvanCampbell <evan.campbell1@unb.ca>
2022-10-11 20:27:22 -0300

    added standard setuptools code

--- setup.py
+++ setup.py
@@ -1,12 +1,44 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# with codecs.open(os.path.join(here,"README.md"), encoding="utf-8") as fh:
+#     long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Extract Features'
+LONG_DESCRIPTION = 'A package that lets you extract features from time series data (emg based).'
 
 setup(
     name="emg_feature_extraction",
-    version="0.0.1",
-    author="Ethan Eddy",
-    author_email="eeddy@unb.ca",
+    version=VERSION,
+    author="Ethan & Evan",
+    description=DESCRIPTION,
+    #long_description_content_type="text/markdown",
+    #long_description=long_description,
     packages=find_packages("src"),
     package_dir={"": "src"},
-    setup_requires=["numpy", "scipy", "sampen"],
-    tests_require=["pytest", "pytest-cov", "numpy", "scipy", "sampen"],
+    install_requires=["numpy", "scipy", "sampen"],
+    keywords=['emg','feature_extraction','pattern_recognition'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
 )
+
+# setup(
+#     name="emg_feature_extraction",
+#     version="0.0.1",
+#     author="Ethan Eddy",
+#     author_email="eeddy@unb.ca",
+#     packages=find_packages("src"),
+#     package_dir={"": "src"},
+#     setup_requires=["numpy", "scipy", "sampen"],
+#     tests_require=["pytest", "pytest-cov", "numpy", "scipy", "sampen"],
+# )
