61a71a31f5a8251f9c2ef78f6e4c7ff5ddc90b19
Ethan Eddy <31893850+eeddy@users.noreply.github.com>
2022-10-12 09:12:38 -0300

    Adding the testing framework (#1)
    * Adding testing
    
    * Added CI back
    
    * Added code coverage
    
    * Added code coverage
    
    * Updates
    
    * Added token
    
    * Updated ci
    
    * Updated to fail build
    
    * Added pytest for mav feature
    
    * Updated CI
    
    * Started testing

---
+++ tests/feature_tests/test_mav_feature.py
@@ -0,0 +1,25 @@
+import pytest
+import numpy as np
+from emg_feature_extraction.feature_extractor import FeatureExtractor as featureextractor
+
+@pytest.fixture(scope='session') 
+def fe():
+    fe = featureextractor(num_channels=8)
+    return fe
+
+def test_normal_case(fe):
+    data = np.loadtxt('tests/data/emg_data_myo.csv', delimiter=',')
+    print(data.shape)
+    windows = [data[0:5,:].transpose()]
+    mav_feat = fe.getMAVfeat(np.array(windows))
+    assert len(mav_feat) == 1
+    assert len(mav_feat[0]) == 8
+    assert list(mav_feat[0]) == list(np.mean(np.abs(data[0:5, :]), axis=0))
+    assert True
+
+def test_zero_case(fe):
+    data = np.loadtxt('tests/data/emg_data_zeros.csv', delimiter=',')
+    windows = fe.get_windows(data, 50, 25)
+    assert len(fe.getMAVfeat(windows)) == len(windows)
+    assert len(fe.getMAVfeat(windows)[0]) == 8
+    assert True
