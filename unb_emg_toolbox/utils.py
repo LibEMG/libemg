6e22a8dcdbf9179915893dbbd19ac6cab9621b97
Evan Campbell <evan.campbell1@unb.ca>
2022-10-19 08:24:25 -0300

    Improving Data Handler for More Inputs (#6)
    * added metadata IN csv examples
    
    * added metadata in csv example
    
    * added mode_windowing (for metadata)
    
    * extended to accept full numpy as metadata not just single element per file.

--- unb_emg_toolbox/utils.py
+++ unb_emg_toolbox/utils.py
@@ -1,3 +1,4 @@
+from asyncio import WindowsProactorEventLoopPolicy
 import numpy as np
 
 def get_windows(data, window_size, window_increment):
@@ -15,6 +16,21 @@ def get_windows(data, window_size, window_increment):
             st_id += window_increment
             ed_id += window_increment
         return np.array(windows)
+
+def get_mode_windows(data, window_size, window_increment):
+    '''
+    data is a NxM stream of data with N samples and M channels (numpy array)
+    window_size is a number of samples in window
+    window_increment is number of samples that advances before next window
+    
+    Rather than capturing an entire window, this function gets the mode of values seen across samples in the window.
+    This is useful for metadata
+    '''
+    windows = get_windows(data, window_size, window_increment)
+    # we want to get the mode along the final dimension
+    mode_of_windows = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=2, arr=windows.astype(np.int64))
+    
+    return mode_of_windows.squeeze()
 
 def make_regex(left_bound, right_bound, values=[]):
     left_bound_str = "(?<="+ left_bound +")"
