4debde8bcb3103e7abab399615b75df05d0e0b93
Ethan Eddy <31893850+eeddy@users.noreply.github.com>
2022-11-01 15:51:26 -0300

    Updated datasets structure (#87)

--- unb_emg_toolbox/datasets.py
+++ unb_emg_toolbox/datasets.py
@@ -1,6 +1,4 @@
-import numpy as np
 import os
-
 from unb_emg_toolbox.data_handler import OfflineDataHandler
 from unb_emg_toolbox.utils import make_regex
 # this assumes you have git downloaded (not pygit, but the command line program git)
@@ -24,9 +22,8 @@ class Dataset:
         else:
             return False
 
-
-
-
+    def prepare_dataset(self, format=OfflineDataHandler):
+        pass
 
 class _3DCDataset(Dataset):
     def __init__(self, save_dir='.', redownload=True, dataset_name="_3DCDataset"):
@@ -42,18 +39,16 @@ class _3DCDataset(Dataset):
             self.github_download(self.url, self.dataset_folder)
 
 
-        
     def prepare_data(self, format=OfflineDataHandler):
-
         if format == OfflineDataHandler:
             sets_values = ["train", "test"]
-            sets_regex = make_regex(left_bound = "\\\\", right_bound="\\\\EMG", values = sets_values)
+            sets_regex = make_regex(left_bound = "/", right_bound="/EMG", values = sets_values)
             classes_values = ["0","1","2","3","4","5","6","7","8","9","10"]
             classes_regex = make_regex(left_bound = "_", right_bound=".txt", values = classes_values)
             reps_values = ["0","1","2","3"]
             reps_regex = make_regex(left_bound = "EMG_gesture_", right_bound="_", values = reps_values)
             subjects_values = ["1","2","3","4","5","6","7","8","9","10","11", "12","13","14","15","16","17","18","19","20","21","22"]
-            subjects_regex = make_regex(left_bound="Participant", right_bound="\\\\",values=subjects_values)
+            subjects_regex = make_regex(left_bound="Participant", right_bound="/",values=subjects_values)
             dic = {
                 "sets": sets_values,
                 "sets_regex": sets_regex,
@@ -65,7 +60,6 @@ class _3DCDataset(Dataset):
                 "subjects_regex": subjects_regex
             }
             odh = OfflineDataHandler()
-            odh.get_data(dataset_folder=self.dataset_folder, dictionary = dic, delimiter=",")
+            odh.get_data(folder_location=self.dataset_folder, filename_dic=dic, delimiter=",")
             return odh
-
         
