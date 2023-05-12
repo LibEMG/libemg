c789c54c0a2efc9600420e9667fa152a29a0bf93
Evan Campbell <evan.campbell1@unb.ca>
2023-05-12 13:01:08 -0400

    Oymotion (#180)
    * added pyserial
    
    * added oymotion
    
    * added oymotion
    
    * created oymotion streamer (8 bits, 1kHz)
    
    * removed hard coded ip and port

--- libemg/_streamers/__init__.py
+++ libemg/_streamers/__init__.py
@@ -1,3 +1,4 @@
 from libemg._streamers import _sifi_streamer
 from libemg._streamers import _myo_streamer
 from libemg._streamers import _delsys_streamer
+from libemg._streamers import _oymotion_streamer
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     #long_description=long_description,
     packages=find_packages(),
     install_requires=["numpy", "scipy", "scikit-learn", "pillow", "matplotlib",
-    "librosa", "wfdb"],
+    "librosa", "wfdb","pyserial"],
     keywords=['emg','feature_extraction','pattern_recognition'],
     classifiers=[
         "Development Status :: 1 - Planning",
