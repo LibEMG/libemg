b136f9e7b1b27283d17b6d2776076af9f01252b5
Evan Campbell <evan.campbell1@unb.ca>
2023-06-26 20:25:50 +0200

    added emager support (#187)

--- libemg/_streamers/__init__.py
+++ libemg/_streamers/__init__.py
@@ -2,3 +2,4 @@ from libemg._streamers import _sifi_streamer
 from libemg._streamers import _myo_streamer
 from libemg._streamers import _delsys_streamer
 from libemg._streamers import _oymotion_streamer
+from libemg._streamers import _emager_streamer
