5f9d548b5b9e865ff9362550ec0f505dc72a856f
Christian Morrell <christian.morrell@breathebiomedical.com>
2023-11-30 17:47:24 -0400

    CartesianPlotAnimator
    Removed the abstraction from PlotAnimator and added a CartesianPlotAnimator class that is inherited by scatter, dot, and target animators. Moved some of the parameters from the method definition to constructors based on whether it was needed for specific implementations.

--- libemg/animator.py
+++ libemg/animator.py
@@ -93,14 +93,11 @@ class Animator:
                 os.remove(filename)
 
 
-class PlotAnimator(ABC, Animator):
+class PlotAnimator(Animator):
     def __init__(self, output_filepath='libemg.gif', fps=24):
         super().__init__(output_filepath, fps)
         self.fpd = fps * 4  # number of frames to generate to travel a distance of 1
-
-    @abstractmethod
-    def plot_icon(self, frame_coordinates, alpha = 1.0, colour = 'black'):
-        raise NotImplementedError('plot_icon() method was not implemented.')
+    
     
     def convert_distance_to_frames(self, coordinates1, coordinates2):
         """Calculate the number of frames needed to move from coordinates1 to coordinates2.
@@ -143,7 +140,188 @@ class PlotAnimator(ABC, Animator):
 
         # Convert the buffer to a PIL Image
         return Image.frombytes('RGBA', canvas.get_width_height(), rgba_buffer)
+    
+    def _format_figure(self):
+        fig = plt.figure()
+        ax = plt.gca()
+        return fig, ax
+    
+    def _preprocess_coordinates(self, coordinates):
+        return coordinates
+    
+    def _show_boundary(self):
+        pass    # going to be different for each implementation, so don't implement it here
 
+    def _show_countdown(self, coordinates, frame_idx):
+        frame_coordinates = coordinates[frame_idx]
+        try:
+            matching_indices = np.where(np.all(coordinates == frame_coordinates, axis=1))[0]
+            future_matching_indices = matching_indices[np.where(matching_indices >= frame_idx)[0]] # only look at indices that are in the future, not the past
+            steady_state_indices = future_matching_indices[np.where(np.diff(future_matching_indices) != 1)[0]]
+            if steady_state_indices.size > 0:
+                # Found end of current steady state
+                final_steady_state_idx = steady_state_indices[0]
+            else:
+                # No other steady states at these coordinates, so just take the end of the current one
+                final_steady_state_idx = future_matching_indices[-1]
+            time_until_movement = (final_steady_state_idx - frame_idx) * self.duration / 1000   # convert from frames to seconds
+            if time_until_movement >= 0.25:
+                # Only show countdown if the steady state is longer than 1 second
+                plt.text(frame_coordinates[0] - 0.03, frame_coordinates[1] - 0.2, str(int(time_until_movement)), fontweight='bold', c='red')
+        except IndexError:
+            # Did not find steady state
+            pass
+    
+    def plot_icon(self, coordinates):
+        plt.plot(coordinates[0], coordinates[1])
+    
+    
+    def make_plot_gif(self, coordinates, title = '', xlabel = '', ylabel = '', save_coordinates = False, 
+                      show_direction = False, show_countdown = False, show_boundary = False, verbose = False):
+        """Save a .gif file of an icon moving around a 2D plane.
+        
+        Parameters
+        ----------
+        coordinates: numpy.ndarray
+            N x M matrix, where N is the number of frames and M is the number of DOFs. Order is x-axis, y-axis, and third DOF (either rotation or target radius).
+            Each row contains the value for x position, y position, and / or third DOF depending on how many DOFs are passed in.
+        title: string (optional), default=''
+            Title of plot.
+        xlabel: string (optional), default=''
+            Label for x-axis.
+        ylabel: string (optional), default=''
+            Label for y-axis.
+        save_coordinates: bool (optional), default=False
+            True if coordinates should be saved to a .txt file for ground truth values, otherwise False.
+        show_direction: bool (optional), default=False
+            True if the direction of the icon should be displayed as a faded icon, otherwise False.
+        show_countdown: bool (optional), default=False
+            True if a countdown should be displayed below the target, otherwise False.
+        show_boundary: bool (optional), default=False
+            True if a circle of radius 1 should be displayed as boundaries, otherwise False.
+        verbose: bool (optional), default=False
+            True if progress should be printed to console, otherwise False.
+        """
+        if save_coordinates:
+            # Save coordinates in .txt file
+            filename_no_extension = os.path.splitext(self.output_filepath)[0]
+            labels_filepath = filename_no_extension + '.txt'
+            np.savetxt(labels_filepath, coordinates, delimiter=',')
+        
+        # Format figure
+        fig, ax = self._format_figure()
+        fig.suptitle(title)
+
+        # Calculate direction changes
+        direction_changes = np.sign(np.diff(coordinates, axis=0, n=1))[:-1] * np.diff(coordinates, axis=0, n=2) / self.duration
+        direction_change_indices = np.where(np.abs(direction_changes) > 1e-8)[0] + 1 # add 1 to align with coordinates
+        direction_change_indices = np.append(direction_change_indices, coordinates.shape[0] - 1)    # append final frame position
+        
+        # Adjust coordinates if desired
+        coordinates = self._preprocess_coordinates(coordinates)
+        
+        frames = []
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        direction_change_idx = None
+        target_alpha = 0.05
+        for frame_idx, frame_coordinates in enumerate(coordinates):
+            if verbose and frame_idx % 10 == 0:
+                print(f'Frame {frame_idx} / {coordinates.shape[0]}')
+            # Plot icon
+            self.plot_icon(frame_coordinates)
+
+            # Format axis
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+            ticks = [-1., -0.5, 0, 0.5, 1.]
+            plt.xticks(ticks)
+            plt.yticks(ticks)
+            plt.axis('equal')
+            ax.set(xlim=xlim, ylim=ylim)    # maintain the same axis limits
+
+            # Plot additional information
+            if show_boundary:
+                # Show boundaries
+                self._show_boundary()
+            
+            if show_direction:
+                # Show path until a change in direction
+                nearest_direction_change_idx = np.where(direction_change_indices - frame_idx >= 0)[0][0]     # get nearest direction change frame that hasn't passed
+                new_direction_change_idx = direction_change_indices[nearest_direction_change_idx]  
+                if new_direction_change_idx != direction_change_idx:
+                    # Update value
+                    direction_change_idx = direction_change_indices[nearest_direction_change_idx]
+                    target_alpha = 0.05  # reset alpha
+                else:
+                    # Add in fade
+                    target_alpha += 0.01
+                    target_alpha = min(0.4, target_alpha) # limit alpha to 0.4
+                self.plot_icon(coordinates[direction_change_idx], alpha=target_alpha, colour='green')
+                
+            if show_countdown:
+                # Show countdown during steady state
+                self._show_countdown(coordinates, frame_idx)
+                
+            # Save frame
+            frame = self._convert_plot_to_image(fig)
+            frames.append(frame)
+            plt.cla()   # clear axis
+        
+        # Save file
+        self.make_gif(frames)
+
+
+class CartesianPlotAnimator(PlotAnimator):
+    def __init__(self, output_filepath = 'libemg.gif', fps = 24, normalize_distance = False, axis_images = None):
+        super().__init__(output_filepath, fps)
+        self.normalize_distance = normalize_distance
+        self.axis_images = axis_images
+        
+
+    def plot_icon(self, frame_coordinates, alpha = 1.0, colour = 'black'):
+        raise NotImplementedError('plot_icon() method was not implemented.')
+    
+    def _format_figure(self):
+        fig = plt.figure(figsize=(8, 8))
+        axis_limits = (-1.25, 1.25)
+        if self.axis_images is not None:
+            axs = self._add_image_label_axes(fig)
+            loc_axis_map = {
+                'NW': axs[0, 0],
+                'N': axs[0, 1],
+                'NE': axs[0, 2],
+                'W': axs[1, 0],
+                'E': axs[1, 2],
+                'SW': axs[2, 0],
+                'S': axs[2, 1],
+                'SE': axs[2, 2]
+            }
+            for loc, image in self.axis_images.items():
+                ax = loc_axis_map[loc]
+                ax.imshow(image)
+            # Set main axis so icon is drawn correctly
+            plt.sca(axs[1, 1])    
+            ax = axs[1, 1]
+        else:
+            ax = plt.gca()
+        ax.set(xlim=axis_limits, ylim=axis_limits)
+        fig.tight_layout()
+        return fig, ax
+    
+    def _preprocess_coordinates(self, coordinates):
+        coordinates = super()._preprocess_coordinates(coordinates)
+
+        if self.normalize_distance:
+            # Normalize to unit circle distance
+            coordinates = self._normalize_to_unit_distance(coordinates[:, 0], coordinates[:, 1])
+        return coordinates
+    
+    def _show_boundary(self):
+        an = np.linspace(0, 2 * np.pi, 100)
+        plt.plot(np.cos(an), np.sin(an), 'b--', alpha=0.7)
+    
+    
     @staticmethod
     def _add_image_label_axes(fig):
         """Add axes to a matplotlib Figure for displaying images in the top, right, bottom, and left of the Figure. 
@@ -188,151 +366,11 @@ class PlotAnimator(ABC, Animator):
         
         assert normalized_coordinates.shape == (x.shape[0], 2)
         return normalized_coordinates
-    
-    def make_plot_gif(self, coordinates, title = '', xlabel = '', ylabel = '', axis_images = None, save_coordinates = False,
-                                 show_direction = False, show_countdown = False, show_boundary = False,
-                                 normalize_distance = False, verbose = False):
-        """Save a .gif file of an icon moving around a 2D plane. Can be used for regression training.
-        
-        Parameters
-        ----------
-        coordinates: numpy.ndarray
-            N x M matrix, where N is the number of frames and M is the number of DOFs. Order is x-axis, y-axis, and third DOF (either rotation or target radius).
-            Each row contains the value for x position, y position, and / or third DOF depending on how many DOFs are passed in.
-        title: string (optional), default=''
-            Title of plot.
-        xlabel: string (optional), default=''
-            Label for x-axis.
-        ylabel: string (optional), default=''
-            Label for y-axis.
-        axis_images: dict (optional), default=None
-            Dictionary mapping compass directions to images. Images will be displayed in the corresponding compass direction (i.e., 'N' correponds to the top of the image).
-            Valid keys are 'NW', 'N', 'NE', 'W', 'E', 'SW', 'S', 'SE'. If None, no images will be displayed.
-        save_coordinates: bool (optional), default=False
-            True if coordinates should be saved to a .txt file for ground truth values, otherwise False.
-        third_dof_display: string (optional), default='size'
-            Determines how the third DOF is displayed. Valid values are 'size' (third DOF is target size), 'rotation' (third DOF is rotation in degrees).
-        show_direction: bool (optional), default=False
-            True if the direction of the icon should be displayed as a faded icon, otherwise False.
-        show_countdown: bool (optional), default=False
-            True if a countdown should be displayed below the target, otherwise False.
-        show_boundary: bool (optional), default=False
-            True if a circle of radius 1 should be displayed as boundaries, otherwise False.
-        normalize_distance: bool (optional), default=False
-            True if the distance between each coordinate should be normalized to 1, otherwise False.
-        verbose: bool (optional), default=False
-            True if progress should be printed to console, otherwise False.
-        """
-        if save_coordinates:
-            # Save coordinates in .txt file
-            filename_no_extension = os.path.splitext(self.output_filepath)[0]
-            labels_filepath = filename_no_extension + '.txt'
-            np.savetxt(labels_filepath, coordinates, delimiter=',')
-        
-        # Calculate direction changes
-        direction_changes = np.sign(np.diff(coordinates, axis=0, n=1))[:-1] * np.diff(coordinates, axis=0, n=2) / self.duration
-        direction_change_indices = np.where(np.abs(direction_changes) > 1e-8)[0] + 1 # add 1 to align with coordinates
-        direction_change_indices = np.append(direction_change_indices, coordinates.shape[0] - 1)    # append final frame position
-
-        if normalize_distance:
-            # Normalize to unit circle distance
-            coordinates = self._normalize_to_unit_distance(coordinates[:, 0], coordinates[:, 1])
-        
-        # Format figure
-        fig = plt.figure(figsize=(8, 8))
-        axis_limits = (-1.25, 1.25)
-        if axis_images is not None:
-            axs = self._add_image_label_axes(fig)
-            loc_axis_map = {
-                'NW': axs[0, 0],
-                'N': axs[0, 1],
-                'NE': axs[0, 2],
-                'W': axs[1, 0],
-                'E': axs[1, 2],
-                'SW': axs[2, 0],
-                'S': axs[2, 1],
-                'SE': axs[2, 2]
-            }
-            for loc, image in axis_images.items():
-                ax = loc_axis_map[loc]
-                ax.imshow(image)
-            # Set main axis so icon is drawn correctly
-            plt.sca(axs[1, 1])    
-            ax = axs[1, 1]
-        else:
-            ax = plt.gca()
-        fig.suptitle(title)
-        fig.tight_layout()
-            
-        frames = []
-        direction_change_idx = None
-        target_alpha = 0.05
-        for frame_idx, frame_coordinates in enumerate(coordinates):
-            if verbose and frame_idx % 10 == 0:
-                print(f'Frame {frame_idx} / {coordinates.shape[0]}')
-            # Plot icon
-            self.plot_icon(frame_coordinates)
-
-            # Format axis
-            plt.xlabel(xlabel)
-            plt.ylabel(ylabel)
-            ticks = [-1., -0.5, 0, 0.5, 1.]
-            plt.xticks(ticks)
-            plt.yticks(ticks)
-            plt.axis('equal')
-            ax.set(xlim=axis_limits, ylim=axis_limits)
-
-            # Show boundaries
-            if show_boundary:
-                an = np.linspace(0, 2 * np.pi, 100)
-                plt.plot(np.cos(an), np.sin(an), 'b--', alpha=0.7)
-            
-            # Plot additional information
-            if show_direction:
-                # Show path until a change in direction
-                nearest_direction_change_idx = np.where(direction_change_indices - frame_idx >= 0)[0][0]     # get nearest direction change frame that hasn't passed
-                new_direction_change_idx = direction_change_indices[nearest_direction_change_idx]  
-                if new_direction_change_idx != direction_change_idx:
-                    # Update value
-                    direction_change_idx = direction_change_indices[nearest_direction_change_idx]
-                    target_alpha = 0.05  # reset alpha
-                else:
-                    # Add in fade
-                    target_alpha += 0.01
-                    target_alpha = min(0.4, target_alpha) # limit alpha to 0.4
-                self.plot_icon(coordinates[direction_change_idx], alpha=target_alpha, colour='green')
-            if show_countdown:
-                # Show countdown below target
-                try:
-                    matching_indices = np.where(np.all(coordinates == frame_coordinates, axis=1))[0]
-                    future_matching_indices = matching_indices[np.where(matching_indices >= frame_idx)[0]] # only look at indices that are in the future, not the past
-                    steady_state_indices = future_matching_indices[np.where(np.diff(future_matching_indices) != 1)[0]]
-                    if steady_state_indices.size > 0:
-                        # Found end of current steady state
-                        final_steady_state_idx = steady_state_indices[0]
-                    else:
-                        # No other steady states at these coordinates, so just take the end of the current one
-                        final_steady_state_idx = future_matching_indices[-1]
-                    time_until_movement = (final_steady_state_idx - frame_idx) * self.duration / 1000   # convert from frames to seconds
-                    if time_until_movement >= 0.25:
-                        # Only show countdown if the steady state is longer than 1 second
-                        plt.text(frame_coordinates[0] - 0.03, frame_coordinates[1] - 0.2, str(int(time_until_movement)), fontweight='bold', c='red')
-                except IndexError:
-                    # Did not find steady state
-                    pass
-                
-            # Save frame
-            frame = self._convert_plot_to_image(fig)
-            frames.append(frame)
-            plt.cla()   # clear axis
-        
-        # Save file
-        self.make_gif(frames)
 
 
-class ScatterPlotAnimator(PlotAnimator):
-    def __init__(self, output_filepath = 'libemg.gif', fps = 24, plot_line = False):
-        super().__init__(output_filepath, fps)
+class ScatterPlotAnimator(CartesianPlotAnimator):
+    def __init__(self, output_filepath = 'libemg.gif', fps = 24, normalize_distance = False, axis_images = None, plot_line = False):
+        super().__init__(output_filepath, fps, normalize_distance, axis_images)
         self.plot_line = plot_line
 
     
@@ -348,7 +386,7 @@ class ScatterPlotAnimator(PlotAnimator):
             plt.plot([0, x], [0, y], c=colour, linewidth=5)
 
 
-class ArrowPlotAnimator(PlotAnimator):
+class ArrowPlotAnimator(CartesianPlotAnimator):
     def plot_icon(self, frame_coordinates, alpha = 1.0, colour = 'black'):
         # Parse coordinates
         x_tail = frame_coordinates[0]
@@ -368,7 +406,7 @@ class ArrowPlotAnimator(PlotAnimator):
         plt.arrow(x_tail, y_tail, x_head - x_tail, y_head - y_tail, head_width=head_size, head_length=head_size, fc=colour, ec=colour, alpha=alpha)
 
 
-class TargetPlotAnimator(PlotAnimator):
+class TargetPlotAnimator(CartesianPlotAnimator):
     @staticmethod
     def _plot_circle(xy, radius, edgecolor, facecolor, alpha = 1.0):
         circle = Circle(xy, radius=radius, edgecolor=edgecolor, facecolor=facecolor, alpha=alpha)
@@ -390,5 +428,3 @@ class TargetPlotAnimator(PlotAnimator):
         TargetPlotAnimator._plot_circle(xy, radius=radius, edgecolor='none', facecolor=colour, alpha = alpha) # plot target
         TargetPlotAnimator._plot_circle(xy, radius=max_radius, edgecolor='black', facecolor='none', alpha=limit_alpha)   # plot max boundary
         TargetPlotAnimator._plot_circle(xy, radius=min_radius, edgecolor='black', facecolor='black', alpha=limit_alpha)   # plot min boundary
-
-
