7c53120344effbcd46db2bfdeaff3d2fb99784db
ECEEvanCampbell <evan.campbell1@unb.ca>
2023-12-05 22:58:50 -0400

    added more args

--- imgui_test.py
+++ imgui_test.py
@@ -1,17 +1,26 @@
 import libemg
 
 if __name__ == "__main__":
-    p = libemg.streamers.sifibridge_streamer(version="1_1")
+    p = libemg.streamers.sifibridge_streamer(version="1_1",
+                                             notch_on=False,
+                                             emg_fir_on=False)
     odh = libemg.data_handler.OnlineDataHandler()
     odh.start_listening()
+
+    # odh.visualize(num_samples=5000)
     # odh.analyze_hardware()
     args = {
-        "odh"         : odh,
-        "media_folder": "media/",
-        "data_folder" : "data/",
-        "num_reps"    : 5,
-        "rep_time"    : 3,
-        "rest_time"   : 1,
-        "auto_advance":True,
+        "online_data_handler"  : odh,
+        "media_folder"         : "images/",
+        "data_folder"          : "data/",
+        "num_reps"             : 5,
+        "rep_time"             : 3,
+        "rest_time"            : 1,
+        "auto_advance"         : True,
+        "window_size"          : 250,
+        "window_inc"           : 150,
+        "features"             : libemg.feature_extractor.FeatureExtractor.get_feature_groups(None)["HTD"],
+        "visualization_horizon": 5000,
+        "visualization_rate"   : 24,
     }
     gui = libemg.gui.GUI(args = args, debug=False)
