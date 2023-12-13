76ae2c49ed2842a06bb42185975e34d6352f5fc3
ECEEvanCampbell <evan.campbell1@unb.ca>
2023-12-13 13:15:27 -0400

    added mp4 support

--- libemg/_gui/_utils.py
+++ libemg/_gui/_utils.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import dearpygui.dearpygui as dpg
 import matplotlib.pyplot as plt
+import cv2
 
 class Media:
     def __init__(self):
@@ -9,14 +10,35 @@ class Media:
 
     def from_file(self, location, fps=24):
         self.type = location.split(".")[-1]
-        self.file_content = Image.open(location)
-        # fps will re-render .png or frames of gif at desired rate
-        # you don't want this large even for .png
         self.fps = fps
-        if self.type == "gif":
-            self.frame = 0
-            self.file_content.seek(self.frame)
-        
+        if self.type == "mp4":
+            self.import_video(location)
+        elif self.type in ["png", "jpg", "bmp"]:
+            self.import_picture(location)
+        elif self.type == "gif":
+            self.import_gif(location)    
+
+    def import_picture(self, location):
+        self.file_content = Image.open(location)
+
+    def import_gif(self, location):
+        self.file_content = Image.open(location)
+        self.frame = 0
+        self.file_content.seek(self.frame)
+        self.n_frames = self.file_content.n_frames
+        self.frame_times = np.linspace(0, self.n_frames/self.fps, int(self.n_frames))
+
+    def import_video(self, location):
+        # get video capture ready
+        self.video_capture = cv2.VideoCapture(location)
+        self.n_frames = self.video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
+        self.frame_times = np.linspace(0, self.n_frames/self.fps, int(self.n_frames))
+        # get first frame
+        self.frame = 0
+        _, cv2_image  = self.video_capture.read()
+        cv2_image = cv2.cvtColor(cv2_image,cv2.COLOR_BGR2RGB)
+        self.file_content =  Image.fromarray(cv2_image)
+
     def from_numpy(self, numpy_array):
         self.file_content = Image.fromarray(numpy_array)
         self.type = "png"
@@ -25,6 +47,10 @@ class Media:
         if self.type == "gif":
             self.frame = 0
             self.file_content.seek(self.frame)
+        if self.type == "mp4":
+            self.frame = 0
+            self.video_capture.set(cv2.CAP_PROP_FRAME_COUNT, 0)
+            self.fps = self.video_capture.get(cv2.CAP_PROP_FPS)
 
     def advance(self):
         assert hasattr(self, "file_content")
@@ -34,7 +60,36 @@ class Media:
                 self.file_content.seek(self.frame)
             else:
                 print("End of gif reached.")
+        if self.type == "mp4":
+            # print(self.frame)
+            self.frame += 1
+            ret, cv2_image  = self.video_capture.read()
+            if not ret:
+                print("End of video reached")
+            else:
+                cv2_image = cv2.cvtColor(cv2_image,cv2.COLOR_BGR2RGB)
+                self.file_content =  Image.fromarray(cv2_image)
     
+    def advance_to(self, play_time):
+        if not hasattr(self, "frame_times"):
+            return
+        # find the closest time
+        del_times = np.abs(self.frame_times - play_time)
+        closest_frame = np.argmin(del_times)
+        if self.type == "gif":
+            if closest_frame < self.file_content.n_frames:
+                self.frame = closest_frame
+                self.file_content.seek(self.frame)
+        if self.type == "mp4":
+            self.frame = closest_frame
+            self.video_capture.set(1, self.frame)
+            ret, cv2_image  = self.video_capture.read()
+            if not ret:
+                print("End of video reached")
+            else:
+                cv2_image = cv2.cvtColor(cv2_image,cv2.COLOR_BGR2RGB)
+                self.file_content =  Image.fromarray(cv2_image)
+
     def get_dpg_formatted_texture(self, width, height, grayscale=False):
         dpg_img = self.file_content.resize((width, height))
         if grayscale:
