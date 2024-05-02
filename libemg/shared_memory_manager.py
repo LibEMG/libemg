dcb624bcfa4c8aba6e91fae44fafae7126ac7747
ECEEvanCampbell <evan.campbell1@unb.ca>
2024-05-01 23:00:21 -0300

    removed unneeded second cleanup

--- libemg/shared_memory_manager.py
+++ libemg/shared_memory_manager.py
@@ -71,11 +71,6 @@ class SharedMemoryManager:
             result.append([k, self.variables[k]["shape"], self.variables[k]["type"]])
         return result
 
-    def cleanup(self):
-        for k in self.variables.keys():
-            self.variables[k]['smh'].close()
-            self.variables[k]['smh'].unlink()
-
 
 
 # if __name__ == "__main__":
