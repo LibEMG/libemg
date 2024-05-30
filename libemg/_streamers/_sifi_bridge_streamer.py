86d74f3772946304276262deb09a56a718312aa5
Gabgagne <darkoumce.gabgagne@gmail.com>
2024-05-30 14:26:49 +0200

    zzz

--- libemg/_streamers/_sifi_bridge_streamer.py
+++ libemg/_streamers/_sifi_bridge_streamer.py
@@ -10,7 +10,7 @@ import re
 
 # this is responsible for receiving the data
 class SiFiBridge:
-    def __init__(self, config, version, other, bridge_version: str | None = "0.6.0"):
+    def __init__(self, config, version, other, bridge_version: str | None = "0.6.1"):
         self.version = version
 
         pltfm = system()
