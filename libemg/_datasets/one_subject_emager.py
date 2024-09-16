9c1f741e4c93b787fcd739496cc134545cc3cd37
Christian Morrell <cmorrell@unb.ca>
2024-09-16 12:45:35 -0300

    Update OneSubjectEMaGerDataset to new format
    Added in OneSubjectEMaGerDataset for regression tasks.
    
    Updated for new Dataset format.

---
+++ libemg/_datasets/one_subject_emager.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from libemg._datasets.dataset import Dataset
+from libemg.data_handler import OfflineDataHandler, RegexFilter, FilePackager
+
+
+class OneSubjectEMaGerDataset(Dataset):
+    def __init__(self, dataset_folder = 'OneSubjectEMaGerDataset/'):
+        super().__init__(
+            sampling=1010,
+            num_channels=64,
+            recording_device='EMaGer',
+            num_subjects=1,
+            gestures={0: 'Hand Close (-) / Hand Open (+)', 1: 'Pronation (-) / Supination (+)'},
+            num_reps=5,
+            description='A simple EMaGer dataset used for regression examples in LibEMG demos.',
+            citation='N/A'
+        )
+        self.url = 'https://github.com/LibEMG/OneSubjectEMaGerDataset'
+        self.dataset_folder = dataset_folder
+
+    def prepare_data(self):
+        if (not self.check_exists(self.dataset_folder)):
+            self.download(self.url, self.dataset_folder)
+        regex_filters = [
+            RegexFilter(left_bound='/', right_bound='/', values=['open-close', 'pro-sup'], description='movements'),
+            RegexFilter(left_bound='_R_', right_bound='_emg.csv', values=[str(idx) for idx in range(self.num_reps)], description='reps')
+        ]
+        package_function = lambda x, y: Path(x).parent.absolute() == Path(y).parent.absolute()
+        metadata_fetchers = [FilePackager(RegexFilter(left_bound='/', right_bound='.txt', values=['labels'], description='labels'), package_function)]
+        odh = OfflineDataHandler()
+        odh.get_data(folder_location=self.dataset_folder, regex_filters=regex_filters, metadata_fetchers=metadata_fetchers)
+        return {'All': odh, 'Train': odh.isolate_data('reps', [0, 1, 2, 3]), 'Test': odh.isolate_data('reps', [4])}
