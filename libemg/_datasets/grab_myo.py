c7a6a12db2fdfcfca2bea9445e1eab0aed3c8bc0
eeddy <eeddy@unb.ca>
2024-09-16 11:04:15 -0300

    Added Grab Myo

--- libemg/_datasets/grab_myo.py
+++ libemg/_datasets/grab_myo.py
@@ -3,29 +3,68 @@ from libemg.data_handler import OfflineDataHandler, RegexFilter
 import os
 
 class GRABMyo(Dataset):
-    def __init__(self):
-        pass
-
-    def __init__(self, save_dir='.', version='1.0.2', redownload=False, subjects=list(range(1,44)), sessions=list(range(1,4)), dataset_name="grabmyo"):
+    """
+    By default this just uses the 16 forearm electrodes. 
+    """
+    def __init__(self, save_dir='.', version='1.0.2', dataset_name="grabmyo", baseline=False):
+        split = '7 Train, 14 Test (2 Seperate Days x 7 Reps)'
+        if baseline:
+            split = '5 Train, 2 Test (Basline)'
         Dataset.__init__(self, 
-                        0, 
-                        0, 
-                        '', 
-                        3, 
-                        [], 
-                        '',
-                        "",
-                        "",
-                        save_dir, redownload)
+                         2048, 
+                         16, 
+                         'EMGUSB2+ device (OT Bioelletronica, Italy)', 
+                         19, 
+                         {0: 'Lateral Prehension', 1: 'Thumb Adduction', 2: 'Thumb and Little Finger Opposition', 3: 'Thumb and Index Finger Opposition', 4: 'Thumb and Index Finger Extension', 5: 'Thumb and Little Finger Extension', 6: 'Index and Middle Finger Extension',
+                            7: 'Little Finger Extension', 8: 'Index Finger Extension', 9: 'Thumb Finger Extension', 10: 'Wrist Extension', 11: 'Wrist Flexion', 12: 'Forearm Supination', 13: 'Forearm Pronation', 14: 'Hand Open', 15: 'Hand Close', 16: 'Rest'},
+                         split, 
+                         "GrabMyo: A large cross session dataset including 17 gestures elicited across 3 seperate sessions.", 
+                         'https://www.nature.com/articles/s41597-022-01836-y')
         self.dataset_name = dataset_name
-        self.dataset_folder = os.path.join(self.save_dir , self.dataset_name, version)
-        
-    def prepare_data(self, subjects=[str(i) for i in range(1,44)], sessions=["1"]):
+        self.dataset_folder = os.path.join(save_dir , self.dataset_name, version)
+
+    def check_if_exist(self):
         if (not self.check_exists(self.dataset_folder)):
             print("Please download the GRABMyo dataset from: https://physionet.org/content/grabmyo/1.0.2/") 
             return 
         print('\nPlease cite: ' + self.citation+'\n')
+
+
+class GRABMyoCrossDay(GRABMyo):
+    def __init__(self, save_dir='.', version='1.0.2', dataset_name="grabmyo"):
+        GRABMyo.__init__(self, save_dir=save_dir, version=version, dataset_name=dataset_name, baseline=False)
+        
+    def prepare_data(self):
+        self.check_exists()
+
         sessions = ["1", "2", "3"]
+        subjects = [str(i) for i in range(1,44)]
+        classes_values = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"]
+        reps_values = ["1","2","3","4","5","6","7"]
+
+        regex_filters = [
+            RegexFilter(left_bound = "session", right_bound="_", values = sessions, description='session'),
+            RegexFilter(left_bound = "_gesture", right_bound="_", values = classes_values, description='classes'),
+            RegexFilter(left_bound = "trial", right_bound=".hea", values = reps_values, description='reps'),
+            RegexFilter(left_bound="participant", right_bound="_",values=subjects, description='subjects')
+        ]
+
+        odh = OfflineDataHandler()
+        odh.get_data(folder_location=self.dataset_folder, regex_filters=regex_filters, delimiter=",")
+
+        forearm_data = odh.isolate_channels(list(range(0,16)))
+        train_data = forearm_data.isolate_data('sets', [0])
+        test_data = forearm_data.isolate_data('sets', [1])
+        return {'All': forearm_data, 'Train': train_data, 'Test': test_data}
+
+class GRABMyoBaseline(GRABMyo):
+    def __init__(self, save_dir='.', version='1.0.2', dataset_name="grabmyo"):
+        GRABMyo.__init__(self, save_dir=save_dir, version=version, dataset_name=dataset_name, baseline=True)
+        
+    def prepare_data(self):
+        self.check_exists()
+
+        sessions = ["1"]
         subjects = [str(i) for i in range(1,44)]
         classes_values = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"]
         reps_values = ["1","2","3","4","5","6","7"]
--- libemg/datasets.py
+++ libemg/datasets.py
@@ -2,7 +2,7 @@ from libemg._datasets._3DC import _3DCDataset
 from libemg._datasets.one_subject_myo import OneSubjectMyoDataset
 from libemg._datasets.emg_epn612 import EMGEPN612
 from libemg._datasets.ciil import CIIL_MinimalData, CIIL_ElectrodeShift
-from libemg._datasets.grab_myo import GRABMyo
+from libemg._datasets.grab_myo import GRABMyoBaseline, GRABMyoCrossDay
 from libemg._datasets.continous_transitions import ContinuousTransitions
 from libemg._datasets.nina_pro import NinaproDB2
 from libemg._datasets.myodisco import MyoDisCo
@@ -22,7 +22,8 @@ def get_dataset_list():
         'EMGEPN612': EMGEPN612,
         'CIIL_MinimalData': CIIL_MinimalData,
         'CIIL_ElectrodeShift': CIIL_ElectrodeShift,
-        'GRABMyo': GRABMyo,
+        'GRABMyoBaseline': GRABMyoBaseline,
+        'GRABMyoCrossDay': GRABMyoCrossDay,
         'ContinuousTransitions': ContinuousTransitions,
         'NinaProDB2': NinaproDB2,
         'MyoDisCo': MyoDisCo,
