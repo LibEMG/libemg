2d58dae0ebee6ec97c444fd167161ab962f30cef
eeddy <eeddy@unb.ca>
2024-09-26 18:20:07 -0300

    Updates

--- dataset_tryout.py
+++ dataset_tryout.py
@@ -1,3 +1,4 @@
 from libemg.datasets import *
 
-evaluate('LDA', 300, 100, feature_list=['MAV','SSC','ZC','WL'], included_datasets=['ContractionIntensity'])
+accs = evaluate('LDA', 300, 100, feature_list=['MAV','SSC','ZC','WL'], included_datasets=['ContractionIntensity'])
+print('\n' + str(accs))
