93d5e40493c22a3ccf5af253e84910c789a25183
eeddy <eeddy@unb.ca>
2024-11-19 18:37:10 -0400

    Fixes

--- libemg/_datasets/emg2pose.py
+++ libemg/_datasets/emg2pose.py
@@ -1,4 +1,5 @@
 from libemg._datasets.dataset import Dataset
+from libemg.data_handler import OfflineDataHandler
 import numpy as np
 import pandas as pd
 import h5py
@@ -14,73 +15,105 @@ class EMG2POSE(Dataset):
                         193, 
                         self.mapping, 
                         'TODO',
-                        "A large dataset from ctrl-labs (Meta) for joint angle estimation.",
+                        "A large dataset from ctrl-labs (Meta) for joint angle estimation. Note that not all subjects have all stages.",
                         "https://openreview.net/forum?id=b5n3lKRLzk")
         self.dataset_folder = dataset_folder
 
-    def prepare_data(self, split = False, subjects = None, train_stages = None, test_stages = None):
+    def check_files(self):
+        if not self.check_exists(self.dataset_folder):
+            print("Please download the dataset from: https://fb-ctrl-oss.s3.amazonaws.com/emg2pose/emg2pose_dataset.tar")
+            return False
+        
+        if not self.check_exists(self.dataset_folder + 'metadata.csv'):
+            print("Could not find metadata file... Please make sure this is downloaded and in the folder.")
+            return False
+        
+        return True
+
+
+    def prepare_data(self, split = False, subjects = None, test_stages = None):
         """Prepares the EMG2POSE dataset.
         
         Parameters
         ----------
         subjects: list
             A list of subject indexes. 
-        train_stages: list (default=first 20)
-            A list of stages to use for training. See self.mapping for options. 
-        test: list (default=last 9)
-            A list of stages to use for testing. See self.mapping for options. 
+        test_stages: list (default=['Wiggling2', 'Gesture3', 'Gesture2', 'Counting2', 'FingerFreeform', 'Counting1'])
+            A list of stages to use for training. See self.mapping for options. If a user doens't have that testing stage then it is ignored.
         """
-        # Make sure all train and test stages are valid 
-        if train_stages:
-            for ts in train_stages:
-                assert ts in list(self.mapping.keys())
-        else:
-            train_stages = list(self.mapping.keys())[0:20]
         if test_stages:
-            for ts in test_stages:
-                assert ts in list(self.mapping.keys())
+            for t in test_stages:
+                assert t in self.mapping.keys()
         else:
-           test_stages = list(self.mapping.keys())[20:29]
+            test_stages = ['Wiggling2', 'Gesture3', 'Gesture2', 'Counting2', 'FingerFreeform', 'Counting1']
+
+        train_stages = []
+        for k in self.mapping.keys():
+            if k not in test_stages:
+                train_stages.append(k)
 
         # (1) Make sure everything is downloaded 
-        if not self.check_exists(self.dataset_folder):
-            print("Please download the dataset from: https://fb-ctrl-oss.s3.amazonaws.com/emg2pose/emg2pose_dataset.tar")
-            return 
-        
-        if not self.check_exists(self.dataset_folder + 'metadata.csv'):
-            print("Could not find metadata file... Please make sure this is downloaded and in the folder.")
+        self.check_files()
 
         # (2) Load metadata file 
         df = pd.read_csv(self.dataset_folder + 'metadata.csv')
-        subject_ids = np.unique(df['user']) # Unique subject IDs 
+        subject_ids = []
+        for user in np.unique(df['user']):
+            if np.unique(df[df['user'] == user]['stage']).shape[0] == 29:
+                subject_ids.append(user)
+        subject_ids = np.array(subject_ids)
+
         if subjects:
             subject_ids = subject_ids[subjects]
-                
+        
+        odh_tr = OfflineDataHandler()
+        odh_tr.subjects = []
+        odh_tr.labels = []
+        odh_tr.stages = []
+        odh_tr.reps = []
+        odh_tr.extra_attributes = ['subjects', 'labels', 'stages', 'reps']
+
+        odh_te = OfflineDataHandler()
+        odh_te.subjects = []
+        odh_te.labels = []
+        odh_te.stages = []
+        odh_te.reps = []
+        odh_te.extra_attributes = ['subjects', 'labels', 'stages', 'reps']
+
         # (3) Iterate through subjects and grab all of the relevant files 
         for s_i, s in enumerate(subject_ids):
             sub_mask = df['user'] == s
             gestures = [self.mapping[v] for v in np.hstack([train_stages, test_stages])]
+            reps = [0] * len(gestures)
             gesture_mask = df['stage'].isin(gestures)
 
-            tr_data = []
-            te_data = []
-            tr_labels = []
-            te_labels = []
             # Get all files for that subject 
             files = df['filename'][(sub_mask) & (gesture_mask)]
             for f in np.array(files):
                 if 'left' in f:
                     continue # Just for now TODO: Fix this !!!!!!!
                 h5_data = h5py.File(self.dataset_folder + '/' + f + '.hdf5', "r")
-                gesture_name = df[df['filename'] == f]['stage'].item()
-                print(gesture_name)
-                print(train_stages)
+                gest = df[df['filename'] == f]['stage'].item()
+                gesture_name = list(self.mapping.keys())[list(self.mapping.values()).index(gest)]
                 if gesture_name in train_stages:
-                    tr_data.append(h5_data['emg2pose']['timeseries']['emg'])
-                    tr_labels.append(h5_data['emg2pose']['timeseries']['joint_angles'])
+                    odh_tr.data.append(h5_data['emg2pose']['timeseries']['emg'])
+                    odh_tr.labels.append(h5_data['emg2pose']['timeseries']['joint_angles'])
+                    odh_tr.stages.append(np.ones((len(odh_tr.data[-1]), 1)) * gestures.index(gest))
+                    odh_tr.subjects.append(np.ones((len(odh_tr.data[-1]), 1)) * s_i)
+                    odh_tr.reps.append(np.ones((len(odh_tr.data[-1]), 1)) * reps[gestures.index(gest)])
                 else:
-                    te_data.append(h5_data['emg2pose']['timeseries']['emg'])
-                    te_labels.append(h5_data['emg2pose']['timeseries']['joint_angles'])
-            
-            print(len(tr_data))
-            print(len(te_data))
+                    odh_te.data.append(h5_data['emg2pose']['timeseries']['emg'])
+                    odh_te.labels.append(h5_data['emg2pose']['timeseries']['joint_angles'])
+                    odh_te.stages.append(np.ones((len(odh_te.data[-1]), 1)) * gestures.index(gest))
+                    odh_te.subjects.append(np.ones((len(odh_te.data[-1]), 1)) * s_i)
+                    odh_te.reps.append(np.ones((len(odh_te.data[-1]), 1)) * reps[gestures.index(gest)])
+                reps[gestures.index(gest)] += 1
+                
+        odh_all = odh_tr + odh_te
+        data = odh_all
+        if split:
+            data = {'All': odh_all, 'Train': odh_tr, 'Test': odh_te}
+        return data 
+        
+    
+
