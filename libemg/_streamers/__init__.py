ea507206bbcf901b8887252e1cea110a85c102d2
eeddy <eeddy@unb.ca>
2025-02-06 08:51:47 -0400

    Updates again

--- libemg/_streamers/__init__.py
+++ libemg/_streamers/__init__.py
@@ -1,13 +1,17 @@
+import sys
+
 from libemg._streamers import _myo_streamer
 from libemg._streamers import _delsys_streamer
 from libemg._streamers import _delsys_API_streamer
-from libemg._streamers import _oymotion_streamer
 from libemg._streamers import _emager_streamer
 from libemg._streamers import _sifi_bridge_streamer
 from libemg._streamers import _OTB_Muovi
 from libemg._streamers import _OTB_MuoviPlus
 from libemg._streamers import _OTB_SessantaquattroPlus
 from libemg._streamers import _OTB_Syncstation
-from libemg._streamers import _oymotion_windows_streamer
+if sys.platform.startswith('win'):
+    from libemg._streamers import _oymotion_windows_streamer
+else:
+    from libemg._streamers import _oymotion_streamer
 from libemg._streamers import _emager_streamer
 from libemg._streamers import _leap_streamer
