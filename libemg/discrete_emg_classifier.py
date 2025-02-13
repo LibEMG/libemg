6be93a43fe37ab9398fc95cc2467c577e77c9462
eeddy <eeddy@unb.ca>
2025-02-12 22:01:14 -0400

    Updates

--- libemg/discrete_emg_classifier.py
+++ libemg/discrete_emg_classifier.py
@@ -1,8 +1,9 @@
 from libemg.feature_extractor import FeatureExtractor
 from multiprocessing import Process
-import numpy as np
-import time
 import pyautogui
+import numpy as np
+import torch
+from playsound import playsound
 from libemg.utils import get_windows
  
 class OnlineEMGDiscreteClassifier:
@@ -19,11 +20,10 @@ class OnlineEMGDiscreteClassifier:
     ip: string (optional), default = '127.0.0.1'
         The ip used for streaming predictions over UDP.
     """
-    def __init__(self, offline_classifiers, online_data_handler, keys, port=12346, ip='127.0.0.1'):
+    def __init__(self, model, online_data_handler, port=12346, ip='127.0.0.1'):
         self.port = port
         self.ip = ip
-        self.keys = keys
-        self.classifiers = offline_classifiers
+        self.model = model
         self.raw_data = online_data_handler.raw_data
         self.process = Process(target=self._run_helper, daemon=True,)
         self.feats = []
@@ -48,30 +48,22 @@ class OnlineEMGDiscreteClassifier:
         self.process.terminate()
  
     def _run_helper(self):
-        from playsound import playsound
+        classes = ['NOTHING', 'CLOSE', 'FLEXION', 'EXTENSION', 'OPEN', 'PINCH']
+        keys = [None, 'C', 'F', 'E', 'O', 'P']
         print("Running Classifier")
         fe = FeatureExtractor()
         self.raw_data.reset_emg()
-        from collections import deque
-        queues = [deque(maxlen=5) for _ in range(0, len(self.classifiers))]
-        prob_queues = [deque(maxlen=5) for _ in range(0, len(self.classifiers))]
         while True:
             # Lets base everything off of EMG
             if len(self.raw_data.get_emg()) >= 260:
                 emg_data = np.array([self.raw_data.get_emg()[-260:]])
-                features = self.get_features(fe, emg_data, 5, 5, ['RMS'])
-                for i, c in enumerate(self.classifiers):
-                    preds, probs = c.predict(features)
-                    queues[i].append(preds[0])
-                    prob_queues[i].append(max(probs[0]))
-                    if len(list(queues[i])) == 5 and sum(list(queues[i])) == 0 and np.mean(list(prob_queues[i])) >= self.raw_data.get_rejection_thresholds()[i]:
-                        self.raw_data.adjust_increment(260, 260)
-                        queues = [deque(maxlen=5) for _ in range(0, len(self.classifiers))]
-                        prob_queues = [deque(maxlen=5) for _ in range(0, len(self.classifiers))]
-                        if self.keys[i] is not None:
-                            pyautogui.press(self.keys[i])
-                        playsound('Other/connect.mp3')
-                    self.raw_data.adjust_increment(260, 5)
+                features = self.get_features(fe, emg_data, 10, 5, ['WENG'], {'WENG_fs': 200})
+                pred = self.model['mlp'].predict(self.model['fe'].forward_once(torch.tensor(features, dtype=torch.float32)))[0]
+                if pred != 0:
+                    self.raw_data.adjust_increment(260, 260)
+                    pyautogui.press(keys[pred])
+                    playsound('Other/connect.mp3')
+                self.raw_data.adjust_increment(260, 10)
  
     def _get_data_helper(self):
         data = np.array(self.raw_data.get_emg())
@@ -83,9 +75,8 @@ class OnlineEMGDiscreteClassifier:
         return data
     
     
-    def get_features(self, fe, data, window_size, window_inc, feats):
+    def get_features(self, fe, data, window_size, window_inc, feats, feat_dic={}):
         data = np.array([get_windows(d, window_size, window_inc) for d in data])
-        feats = np.array([fe.extract_features(feats, d, array=True) for d in data])
+        feats = np.array([fe.extract_features(feats, d, array=True, feature_dic=feat_dic) for d in data])
         feats = np.nan_to_num(feats, copy=True, nan=0, posinf=0, neginf=0)
         return feats
-    
