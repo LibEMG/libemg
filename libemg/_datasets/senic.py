f8ec14fa7599e63b16cc9ea0cf33bdf014848114
eeddy <eeddy@unb.ca>
2025-03-06 15:17:37 -0400

    Added code to parse first 10 subjects

--- libemg/_datasets/senic.py
+++ libemg/_datasets/senic.py
@@ -33,27 +33,24 @@ class SeNic(Dataset):
         for f in filenames:
             if '.rar' in f:
                 print("Unpack all rars manually!")
-                # print(self.dataset_folder + f)
-                # with rarfile.RarFile(self.dataset_folder + f) as rf:
-                #     rf.extractall(self.dataset_folder)
-                # # os.remove(self.dataset_folder + f)
-        return None 
+                # TODO: Amir - figure out how to unpack rars automatically. 
 
-        # sets_values = ["1","2","3","4","5","6"]
-        # classes_values = ["0","1","2","3","4"]
-        # reps_values = ["0","1"]
-        # regex_filters = [
-        #     RegexFilter(left_bound = "/trial_", right_bound="/", values = sets_values, description='sets'),
-        #     RegexFilter(left_bound = "C_", right_bound=".csv", values = classes_values, description='classes'),
-        #     RegexFilter(left_bound = "R_", right_bound="_", values = reps_values, description='reps')
-        # ]
-        # odh = OfflineDataHandler()
-        # odh.get_data(folder_location=self.dataset_folder, regex_filters=regex_filters, delimiter=",")
-        # odh.subjects = []
-        # odh.subjects = [np.zeros((len(d), 1)) for d in odh.data]
-        # odh.extra_attributes.append('subjects')
-        # data = odh
-        # if split:
-        #     data = {'All': odh, 'Train': odh.isolate_data("sets", [0,1,2,3,4], fast=True), 'Test': odh.isolate_data("sets", [5,6], fast=True)}
+        sets_values = [str(i) for i in range(0, 10)]
+        subjects_values = [str(i) for i in range(0,6)]
+        positions_values = [str(i) for i in range(0, 10)]
+        classes_values = ['eversion', 'fist', 'open_hand', 'pinch_forefinger', 'pinch_middlefinger', 'two', 'varus']
+        reps_values = ['0','1','2']
 
-        # return data
+        regex_filters = [
+            RegexFilter(left_bound = "h", right_bound="/", values = subjects_values, description='subjects'),
+            RegexFilter(left_bound = "/", right_bound="/emg_", values = sets_values, description='days'),
+            RegexFilter(left_bound = "_p", right_bound="_r", values = positions_values, description='positions'),
+            RegexFilter(left_bound = "_", right_bound=".csv", values = classes_values, description='classes'),
+            RegexFilter(left_bound = "_r", right_bound="_", values = reps_values, description='reps')
+        ]
+
+        # TODO: We need to active threshold because there is not an explicit NM class 
+
+        odh = OfflineDataHandler()
+        odh.get_data(folder_location=self.dataset_folder, regex_filters=regex_filters, delimiter=",")
+        return odh
