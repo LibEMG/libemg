9ab88b3c0eda2f3febbbde7b57f1c537094d3f98
Christian Morrell <cmorrell@unb.ca>
2025-03-28 18:40:56 -0300

    Revert "Move private method with others"
    This reverts commit 4a588ef361221681f2c8a8f5e20574537ea103a0.

--- libemg/emg_predictor.py
+++ libemg/emg_predictor.py
@@ -983,7 +983,7 @@ class OnlineStreamer(ABC):
                 continue
 
             # Prediction/Postprocessing stage
-            raw = self.prediction_function_handle(model_input)
+            raw = self.prediction_function_handle(model_input, window)
             processed = self.postprocessing_function_handle(raw, model_input, window)
             info = self.format_output_info(processed, model_input, window)
             for writer in self.output_writers:
@@ -1100,7 +1100,7 @@ class OnlineEMGClassifier(OnlineStreamer):
         super(OnlineEMGClassifier, self).__init__(offline_classifier, window_size, window_increment, online_data_handler,
                                                   file_path, file, smm, smm_items, features, std_out, output_writers)
         self.previous_predictions = deque(maxlen=self.predictor.majority_vote)
-        self.previous_prediction_probabilities = deque(maxlen=self.predictor.majority_vote)
+        self.previous_probabilities = deque(maxlen=self.predictor.majority_vote)
         self.smi = smm_items
 
         # TODO: remove output_format. it doesn't make much sense to me that we have this and output_writers 
@@ -1109,15 +1109,14 @@ class OnlineEMGClassifier(OnlineStreamer):
     def default_prediction_function(self, model_input: np.ndarray) -> Tuple[Any, Any]:
         probabilities = self.predictor._predict_proba(model_input)
         prediction, probability = self.predictor._prediction_helper(probabilities)
-        prediction = prediction[0]
-        probability = probability[0]
         self.previous_predictions.append(prediction)
-        self.previous_prediction_probabilities.append(probability)
-        return (prediction, probabilities[0])
+        self.previous_probabilities.append(probability)
+        return (prediction[0], probability[0])
+
 
     def default_postprocessing_function(self, raw: Any, model_input: np.ndarray, window: Dict[str, Any]):
-        prediction, probabilities = raw
-        predictions = self.predictor._apply_post_processing(list(self.previous_predictions), list(self.previous_prediction_probabilities))
+        prediction, probability = raw
+        predictions = self.predictor._apply_post_processing(list(self.previous_predictions), list(self.previous_probabilities))
         prediction = predictions[-1]    # double check you want the last one
         # TODO: Write an assertion to make sure this gives the same result as before
         # if self.predictor.rejection:
@@ -1131,30 +1130,21 @@ class OnlineEMGClassifier(OnlineStreamer):
             calculated_velocity = " 0"
             if prediction >= 0:
                 calculated_velocity = " " + str(self.predictor._get_velocity(window, prediction))
-        return (prediction, probabilities, calculated_velocity)
+        return (prediction, probability, calculated_velocity)
 
     def format_output_info(self, 
                            processed:   Tuple[Any, Any, Any],
                            model_input: Any, 
                            window:      Dict[str, Any]) -> Dict[str, Any]:
         # Compose a dictionary with all information you wish to send.
-        prediction, probabilities, calculated_velocity = processed
-        if isinstance(prediction, np.ndarray):
-            prediction = prediction.item()
-        timestamp = time.time()
-        if self.output_format == 'predictions':
-            message = str(prediction) + calculated_velocity + '\n'
-        else:
-            message = ' '.join([f'{i:.2f}' for i in probabilities]) + calculated_velocity + " " + str(timestamp)
-
+        prediction, probability, calculated_velocity = processed
         info = {
-            "timestamp": timestamp,
+            "timestamp": time.time(),
             "model_output": prediction,
-            "probability": probabilities,
+            "probability": probability,
             "velocity": calculated_velocity,
             "model_input": model_input,
-            "window": window,
-            "message": message
+            "window": window
         }
         return info
 
