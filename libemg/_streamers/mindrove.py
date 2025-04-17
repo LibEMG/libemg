fe848a050b54c743a9c1e73e169683c9ed125160
Christian Morrell <cmorrell@unb.ca>
2025-04-17 14:47:04 -0300

    Add MindroveStreamer

---
+++ libemg/_streamers/mindrove.py
@@ -0,0 +1,50 @@
+from multiprocessing import Process
+
+import numpy as np
+from mindrove.board_shim import BoardShim, MindRoveInputParams, BoardIds
+
+from libemg.shared_memory_manager import SharedMemoryManager
+
+
+class MindroveStreamer(Process):
+    def __init__(self, shared_memory_items):
+        super().__init__(daemon=True)
+        self.shared_memory_items = shared_memory_items
+
+    def run(self):
+        def add_to_buffer(buffer, new_data):
+            new_buffer = np.vstack((new_data, buffer))  # put new data on top
+            new_buffer = new_buffer[:buffer.shape[0], :]    # ensure buffer stays the same size
+            return new_buffer
+
+        # Initialize shared memory in this process
+        smm = SharedMemoryManager()
+        for item in self.shared_memory_items:
+            smm.create_variable(*item)
+
+        params = MindRoveInputParams()
+        board_id = BoardIds.MINDROVE_WIFI_BOARD
+        board_shim = BoardShim(board_id, params)
+        board_shim.prepare_session()
+        board_shim.start_stream()
+        # window_size_ms = 100
+        # sampling_rate = board_shim.get_sampling_rate(board_id)
+        # num_samples = int(window_size_ms / 1000 * sampling_rate)
+        num_samples = 1 # number of samples to grab and add to buffer at a time
+        emg_channels = board_shim.get_emg_channels(board_id)
+
+        while True:
+            data = board_shim.get_board_data(num_samples=num_samples)   # grabs data from ringbuffer AND DELETES IT
+            if data is None:
+                continue
+
+            # data is of shape: (num_params, num_samples)
+            emg = data[emg_channels].T  # we expect data as (num_samples, num_channels)
+            print(emg.shape)
+            smm.modify_variable('emg', add_to_buffer)
+            smm.modify_variable('emg_count', lambda x: x + 1)
+
+        # TODO: Call cleanup somehow
+        if board_shim.is_prepared():
+            board_shim.release_session()
+
