51c764ab70fb02e908a18be8a2c98fdd2bc2775f
bella-montanaro <isabellamontanaro2030@u.northwestern.edu>
2026-09-10 17:00:57 -0500

    Add EMGData16ch dataset wrapper

--- libemg/datasets.py
+++ libemg/datasets.py
@@ -20,6 +20,7 @@ from libemg.offline_metrics import OfflineMetrics
 from libemg.filtering import Filter
 from libemg._datasets.emg2pose import EMG2POSEUD, EMG2POSECU
 from libemg._datasets.emg_epn100 import EMGEPN100
+from libemg._datasets.emgdata_16ch import EMGData16ch
 from sklearn.preprocessing import StandardScaler
 import pickle
 import numpy as np
@@ -72,6 +73,7 @@ def get_dataset_list(type='CLASSIFICATION', cross_user=False):
         'TMR_Pre': TMR_Pre,
         'HyserPR': HyserPR,
         'EMGEPN100': EMGEPN100,
+        'EMGData16ch': EMGData16ch
     }
 
     regression = {
